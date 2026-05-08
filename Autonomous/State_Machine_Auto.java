aad5498e1abcf557f0fa66cfbe360e3ff654d9d1
aSlicedOrange <terkahubert@gmail.com>
2026-05-08 13:20:09 +0100

    Add odometry driver initialization and configuration
    Initialize odometry driver and configure settings for robot movement.

--- Autonomous/State_Machine_Auto.java
+++ Autonomous/State_Machine_Auto.java
@@ -21,6 +21,8 @@ public class SM_Auto extends OpMode{
     private DcMotorEx flywheelMotorL;
     private DcMotorEx flywheelMotorR;
 
+    GoBildaPinpointDriver odo;
+
 
     private enum state {
         MOVE_TO_SHOOT,
@@ -31,7 +33,50 @@ public class SM_Auto extends OpMode{
 
     @Override
     public void init(){
-      
+        odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo")
+
+        //Odometry Computer Configuration
+        odo.setOffsets( , ); //Offsets for where the opometry pods are on the robot
+        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWING_ARM); //Check if correct, for what hardware ur using
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD); //to tell encoder what positive direction is
+
+        odo.resetPosAndIMU();
+        Pose2D startingPosition = new Pose2D(DistanceUnit.MM, x, y, AngleUnit.RADIANS, 0)//Set the x and y values for the starting pos (depends on where you start) and heading
+        odo.setPosition(startingPosition)
+
+    }
+    
+    public double getRotatePower(double heading) {
+        
+        
+    }
+    
+    public void moveRobot(double forward, double strafe, double heading) {
+        
+        double rotate = ;
+
+        
+        pose2D pos = odo.getPosition();
+        double heading = pos.getHeading(AngleUnit.RADIANS);
+
+        double cosAngle = Math.cos((Math.PI / 2) - heading);
+        double sinAngle = Math.sin((Math.PI / 2) - heading);
+
+        double globalStrafe = -forward * sinAngle + strafe * cosAngle;
+        double globalForward = forward * sinAngle + strafe * cosAngle;
+
+        //Array for drive motors
+        double[] newWheelSpeeds = new double[4];
+
+        newWheelSpeeds[0] = globalForward + globalStrafe + rotate;
+        newWheelSpeeds[1] = globalForward - globalStrafe - rotate;
+        newWheelSpeeds[2] = globalForward - globalStrafe + rotate;
+        newWheelSpeeds[3] = globalForward + globalStrafe - rotate;
+
+        frontLeft.setPower(newWheelSpeeds[0]);
+        frontRight.setPower(newWheelSpeeds[1]);
+        backLeft.setPower(newWheelSpeeds[2]);
+        backRight.setPower(newWheelSpeeds[3]);
     }
 
     @Override
