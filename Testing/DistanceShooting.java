89e9d2f84411999570822c8c9cdc9484f6c0a64c
aSlicedOrange <terkahubert@gmail.com>
2026-05-13 16:35:14 +0100

    Create DistanceShooting.java

---
+++ Testing/DistanceShooting.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.teamcode.Classes.Flywheel;
+
+
+@TeleOp(name = "Motor Tester")
+public class MotorTester extends OpMode{
+
+
+    private DcMotor motor1;
+    private DcMotor motor2;
+
+    Flywheel flywheel;
+    
+    private double goalHeight = 1.15;
+    private double gravity = 9.816;
+    private double Uy = (2*gravity*goalHeight)**0.5;
+    private double Ux;
+    private double backplateAngle;
+    private double flywheelToBallDiv = 1;
+  
+    public double distance(double distance) {
+        Ux = (distance*gravity**0.5) / (2*goalHeight)**0.5;
+        backplateAngle = Math.atan(Uy / Ux);
+
+        
+    }
+  
+    @Override
+    public void init(){
+        flywheel = new Flywheel(hardwareMap);
+      
+        motor1 = hardwareMap.get(DcMotorEx.class, "Motor1");
+        motor2 = hardwareMap.get(DcMotor.class, "Motor2");
+      
+        motor1.setZeroPowerBehavior(DcMotorEX.ZeroPowerBehavior.FLOAT);
+        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        
+        motor1.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop(){
+    
+    if (gamepad1.right_trigger > 0.1) {
+        motor1.setVelocity(distance(2));
+    } else {
+        motor1.setPower(0);
+    }
+    if (gamepad1.left_trigger > 0.1) {
+        motor2.setPower(gamepad1.left_trigger);
+    } else {
+        motor2.setPower(0);
+    }
+    
+    
+    
+    }
+}
