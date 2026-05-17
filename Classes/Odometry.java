ad0e62f626f919ec781f85d56075ced81f7a1df9
aSlicedOrange <terkahubert@gmail.com>
2026-05-18 00:12:57 +0100

    Update Odometry.java

--- Classes/Odometry.java
+++ Classes/Odometry.java
@@ -10,10 +10,10 @@ public class Odometry {
     private GoBildaPinpointDriver odo; 
     private Pose2D pos;
 
-  public Odometry(HardwareMap hardwareMap) {
-      init(hardwareMap);
+  public Odometry(HardwareMap hardwareMap, double initialX, double initialY, double initialHeading) {
+      init(hardwareMap, initialX, initialY, initialHeading);
   }
-  public void init(HardwareMap hardwareMap) {
+  public void init(HardwareMap hardwareMap, double initialX, double initialY, double initialHeading) {
       odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
 
       odo.setOffsets(-7.7008210429995145, 4.118370236374258, DistanceUnit.INCH); 
@@ -21,7 +21,7 @@ public class Odometry {
       odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD); 
 
       odo.resetPosAndIMU();
-      Pose2D startingPosition = new Pose2D(DistanceUnit.MM, 0, 0, AngleUnit.RADIANS, 0);
+      Pose2D startingPosition = new Pose2D(DistanceUnit.MM, initialX, initialY, AngleUnit.RADIANS, initialHeading);
       odo.setPosition(startingPosition);
   }
 
