b600374dd2cf606efd426d2b7abc3e978b329cde
aSlicedOrange <terkahubert@gmail.com>
2026-05-18 01:09:59 +0100

    Update Odometry.java

--- Classes/Odometry.java
+++ Classes/Odometry.java
@@ -10,35 +10,39 @@ public class Odometry {
     private GoBildaPinpointDriver odo; 
     private Pose2D pos;
 
-  public Odometry(HardwareMap hardwareMap, double initialX, double initialY, double initialHeading) {
-      init(hardwareMap, initialX, initialY, initialHeading);
-  }
-  public void init(HardwareMap hardwareMap, double initialX, double initialY, double initialHeading) {
-      odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
+    public Odometry(HardwareMap hardwareMap, double initialX, double initialY, double initialHeading) {
+        init(hardwareMap, initialX, initialY, initialHeading);
+    }
+    public void init(HardwareMap hardwareMap, double initialX, double initialY, double initialHeading) {
+        odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
 
-      odo.setOffsets(-7.7008210429995145, 4.118370236374258, DistanceUnit.INCH); 
-      odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD); 
-      odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD); 
+        odo.setOffsets(-7.7008210429995145, 4.118370236374258, DistanceUnit.INCH); 
+        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD); 
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD); 
 
-      odo.resetPosAndIMU();
-      Pose2D startingPosition = new Pose2D(DistanceUnit.MM, initialX, initialY, AngleUnit.RADIANS, initialHeading);
-      odo.setPosition(startingPosition);
-  }
+        odo.resetPosAndIMU();
+        Pose2D startingPosition = new Pose2D(DistanceUnit.MM, initialX, initialY, AngleUnit.RADIANS, initialHeading);
+        odo.setPosition(startingPosition);
+    }
 
-  public void update() {
-      odo.update();
-      pos = odo.getPosition();
-  }
-    
-  public double getX() {
-      return pos.getX(DistanceUnit.MM);
-  }
+    public void update() {
+        odo.update();
+        pos = odo.getPosition();
+    }
 
-  public double getY() {
-      return pos.getY(DistanceUnit.MM);
-  }
+    public Pose2D getPosition() {
+        return pos;
+    }
 
-  public double getHeadingRadians() {
-      return pos.getHeading(AngleUnit.RADIANS);
-  }
+    public double getX() {
+        return pos.getX(DistanceUnit.MM);
+    }
+
+    public double getY() {
+        return pos.getY(DistanceUnit.MM);
+    }
+
+    public double getHeadingRadians() {
+        return pos.getHeading(AngleUnit.RADIANS);
+    }
 }
