fabbe83cbe7aeddbf2b89519fc8e0211ad9162d4
aSlicedOrange <terkahubert@gmail.com>
2026-05-19 13:19:35 +0100

    Import Flywheel class for PIDF tuning

--- Testing/Flywheel_PIDF_Tuning.java
+++ Testing/Flywheel_PIDF_Tuning.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.teamcode.Classes.Flywheel;
 
 
 @TeleOp(name = "Flywheel PIDF Tuning")
