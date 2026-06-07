9b1b6d08f406eb682e4bf1bf2c4285205a075ce2
aSlicedOrange <terkahubert@gmail.com>
2026-06-07 23:40:24 +0100

    Update State_Machine_Auto.java

--- Autonomous/State_Machine_Auto.java
+++ Autonomous/State_Machine_Auto.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import java.lang.reflect.Array;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -15,6 +16,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.teamcode.Classes.Flywheel;
 import org.firstinspires.ftc.teamcode.Classes.Odometry;
 
+
 @Autonomous(name = "SM_Auto")
 public class State_Machine_Auto extends OpMode{
 
@@ -28,7 +30,32 @@ public class State_Machine_Auto extends OpMode{
     
     Odometry odo;
 
+    //P Values
+    double minDegreeDifference = 0.1;
+    double minForwardDifference = 10.0;
+    double minStrafeDifference = 10.0;
+    
+    double divRotateDifference = 30.0;
+    double divForwardDifference = 300.0;
+    double divStrafeDifference = 300.0;
+    
+    double targetMoveHeading;
+    double targetMoveX;
+    double targetMoveY;
+    
+    double[][] chains = {{1000, 1000, 0}, {-1000, -1000, -90}, {2000, 0, 180}, {0, 0, 0}};
+    double[][] chainsError = {{250, 250, 0.5}, {250, 250, 0.5}, {15, 15, 0.25}, {10, 10, 0.1}};
+    
+    
+    double[][] chainsModular = new double[5][5];
+    double[][] chainsErrorModular = new double[5][5];
+    double[] chainsErrorModularLow = {10, 10, 0.1};
+    double[] chainsErrorModularMedium = ;
+    double[] chainsErrorModularHigh = ;
+    double[] currentErrorModularState = {250, 250, 0.25};
+    double[] ErrorModularStates = {{10, 10, 0.1}, {250, 250, 0.25}, {500, 500, 0.5}}
 
+    //Enums
     private enum movementState {
         MOVE_TO_SHOOT,
         MOVE_TO_GATE,
@@ -42,33 +69,25 @@ public class State_Machine_Auto extends OpMode{
         INTAKE_ON,
         INTAKE_OFF
     }
-
-    private enum testStates {
+    
+    private enum controlState {
         MOVE_TO_POS,
-        SAVE_POS,
+        MOVE_TO_CHAIN,
         NONE
     }
+    
+    private controlState currentState = controlState.NONE;
 
-    private testStates currentState = testStates.NONE;
-
-    double targetMoveX = 0;
-    double targetMoveY = 0;
-    double targetMoveHeading = 0;
-
-    //For Testing
-    private double deltaHeading = 0;
-    private double deltaX = 0;
-    private double deltaY = 0;
-
+    double currentX;
+    double currentY;
+    double currentHeading;
+    
     private com.qualcomm.robotcore.hardware.Gamepad currentGamepad1 = new com.qualcomm.robotcore.hardware.Gamepad();
     private com.qualcomm.robotcore.hardware.Gamepad previousGamepad1 = new com.qualcomm.robotcore.hardware.Gamepad();
 
     public double[] getRotatePower(double currentHeading, double targetHeading) {
-        double minDegreeDifference = 0.5;
         double errorHeading = targetHeading - currentHeading;
-
         
-        double divRotateDifference = 10.0;
         double divRatio = errorHeading / divRotateDifference;
         double maxPower = 1;
         
@@ -87,18 +106,15 @@ public class State_Machine_Auto extends OpMode{
     }
 
     public double[] getForwardPower(double currentY, double targetY) {
-        double minYDifference = 5.0;
-        
         double errorY = targetY - currentY; 
-        double divYDifference = 150.0;
-        double divRatio = errorY / divYDifference;
+        double divRatio = errorY / divForwardDifference;
         double maxPower = 1;
 
         double motorPower = Math.min(Math.abs(divRatio), maxPower) * Math.signum(errorY);
 
         
         double[] forwardMotorPower = {0.0, 0.0, 0.0, 0.0};
-        if (Math.abs(errorY) > minYDifference) {
+        if (Math.abs(errorY) > minForwardDifference) {
             forwardMotorPower[0] = motorPower; //fL
             forwardMotorPower[1] = motorPower; //fR
             forwardMotorPower[2] = motorPower; //bL
@@ -109,20 +125,17 @@ public class State_Machine_Auto extends OpMode{
 
     }
 
-
     public double[] getStrafePower(double currentX, double targetX) {
-        double minXDifference = 5.0;
         double errorX = targetX - currentX;
-
-        double divXDifference = 150.0;
-        double divRatio = errorX / divXDifference;
+        
+        double divRatio = errorX / divStrafeDifference;
         double maxPower = 1;
 
         double motorPower = Math.min(Math.abs(divRatio), maxPower) * Math.signum(errorX);
 
         
         double[] strafeMotorPower = {0.0, 0.0, 0.0, 0.0};
-        if (!(Math.abs(errorX) < minXDifference)) {
+        if (!(Math.abs(errorX) < minStrafeDifference)) {
             strafeMotorPower[0] = motorPower; //fL
             strafeMotorPower[1] = -motorPower; //fR
             strafeMotorPower[2] = -motorPower; //bL
@@ -134,10 +147,6 @@ public class State_Machine_Auto extends OpMode{
     }
     
     public boolean moveRobot(double targetX, double targetY, double targetHeading) {
-        double currentHeading = odo.getHeading(AngleUnit.DEGREES);
-        double currentX = -odo.getY();
-        double currentY = odo.getX();
-
         double fieldErrorX = targetX - currentX;
         double fieldErrorY = targetY - currentY;
     
@@ -186,12 +195,47 @@ public class State_Machine_Auto extends OpMode{
         }
     }
     
+    public double[][][] chain(double[][] chainNodes, double[][] errors) {
+        double errorX = Math.abs(chainNodes[0][0]) - Math.abs(currentX);
+        double errorY = Math.abs(chainNodes[0][1]) - Math.abs(currentX);
+        double errorHeading = Math.abs(chainNodes[0][2]) - Math.abs(currentX);
+        
+    double[][] chainNodesNew = new double[chainNodes.length - 1][3];
+    double[][] errorsNew = new double[errors.length - 1][3];
+        
+        
+        if (errorX <= errors[0][0] && errorY <= errors[0][1] && errorHeading <= errors[0][2]) {
+            for (int i = 1; i < chainNodes.length; i++) {
+                chainNodesNew[i - 1][0] = chainNodes[i][0];
+                chainNodesNew[i - 1][1] = chainNodes[i][1];
+                chainNodesNew[i - 1][2] = chainNodes[i][2];
+            }
+            chainNodes = chainNodesNew;
+            
+            for (int i = 1; i < errors.length; i++) {
+                errorsNew[i - 1][0] = errors[i][0];
+                errorsNew[i - 1][1] = errors[i][1];
+                errorsNew[i - 1][2] = errors[i][2];
+            }
+            errors = errorsNew;
+        }
+    
+        if (chainNodes.length > 0 && errors.length > 0) {
+            moveRobot(chainNodes[0][0], chainNodes[0][1], chainNodes[0][2]);
+        }
+        double[][][] result = {chainNodes, errors};
+        return result;
+    }
+    
     @Override
     public void init(){
 
         flywheel = new Flywheel(hardwareMap);
 
         odo = new Odometry(hardwareMap, 0, 0, 0);
+        currentX = 0;
+        currentY = 0;
+        currentHeading = 0;
         
         frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
         backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
@@ -203,83 +247,82 @@ public class State_Machine_Auto extends OpMode{
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         
     }
     @Override
     public void loop(){
         //Testing code
         odo.update();
-        double currentHeading = odo.getHeading(AngleUnit.DEGREES);
-        double currentX = -odo.getY();
-        double currentY = odo.getX();
+        currentHeading = -odo.getHeading(AngleUnit.DEGREES);
+        currentX = odo.getY();
+        currentY = -odo.getX();
+        
 
+        if (gamepad1.a) {
+            currentState = controlState.MOVE_TO_POS;
+        } else if (gamepad1.b) {
+            currentState = controlState.MOVE_TO_CHAIN;
+        } else if (gamepad1.y) {
+            currentState = controlState.NONE;
+        }
         
         previousGamepad1.copy(currentGamepad1);
         currentGamepad1.copy(gamepad1);
         
-        if (currentGamepad1.dpad_left && !previousGamepad1.dpad_left) {
-            deltaX -= 200;
-        }
-        if (currentGamepad1.dpad_right && !previousGamepad1.dpad_right) {
-            deltaX += 200;
-        }
-        
-        if (currentGamepad1.dpad_up && !previousGamepad1.dpad_up) {
-            deltaY += 200;
-        }
-        if (currentGamepad1.dpad_down && !previousGamepad1.dpad_down) {
-            deltaY -= 200;
-        }
-        
-        if (currentGamepad1.left_bumper && !previousGamepad1.left_bumper) {
-            deltaHeading -= 10;
-        }
-        if (currentGamepad1.right_bumper && !previousGamepad1.right_bumper) {
-            deltaHeading += 10;
-        }
-
-
-    
-        
-        
-        telemetry.addData("Delta X:", deltaX);
-        telemetry.addData("Delta Y:", deltaY);
-        telemetry.addData("Delta Heading:", deltaHeading);
-
-        if (currentGamepad1.a && !previousGamepad1.a) {
-            currentState = testStates.SAVE_POS;
-        } else if (gamepad1.y) {
-            currentState = testStates.NONE;
-        } else if (gamepad1.x) {
-            deltaX = 0;
-            deltaY = 0;
-            deltaHeading = 0;
+        if (currentGamepad1.dpad_up && !previousGamepad1.dpad_up && chainsModular.length<5) {
+            chainsModular[chainsModular.length][0] = currentX;
+            chainsModular[chainsModular.length][0] = currentX;
+            chainsModular[chainsModular.length][0] = currentX;
+            
+        } else if (currentGamepad1.dpad_down && !previousGamepad1.dpad_down && chainsModular.length>0) {
+            double[][] chainsModularTemp;
+            double[][] chainsErrorModularTemp;
+            for (int i = 1; i < chainsModular.length; i++) {
+                chainsModularTemp[i - 1][0] = chainsModular[i][0];
+                chainsModularTemp[i - 1][1] = chainsModular[i][1];
+                chainsModularTemp[i - 1][2] = chainsModular[i][2];
+            }
+            for (int i = 1; i < chainsErrorModular.length; i++) {
+                chainsErrorModularTemp[i - 1][0] = chainsErrorModular[i][0];
+                chainsErrorModularTemp[i - 1][1] = chainsErrorModular[i][1];
+                chainsErrorModularTemp[i - 1][2] = chainsErrorModular[i][2];
+            }
+            chainsModular = chainsModularTemp;
+            chainsErrorModular = chainsErrorModularTemp;
         }
 
         switch (currentState) {
             case MOVE_TO_POS:
-                if (!(moveRobot(targetMoveX, targetMoveY, targetMoveHeading))) {
+                if (!(moveRobot(0, 0, 0))) {
                     telemetry.addLine("Moving to Position...");
-                    telemetry.addData("X Data", "Current: %.1f | Target: %.1f", currentX, targetMoveX);
-                    telemetry.addData("Y Data", "Current: %.1f | Target: %.1f", currentY, targetMoveY);
-                    telemetry.addData("Heading Data", "Current: %.1f | Target: %.1f", currentHeading, targetMoveHeading);
+                    telemetry.addData("X Data", "Current: %.1f | Target: %.1f | Difference * 100: %.1f", currentX, targetMoveX, 100*(targetMoveX-currentX));
+                    telemetry.addData("Y Data", "Current: %.1f | Target: %.1f | Difference * 100: %.1f", currentY, targetMoveY, 100*(targetMoveY-currentY));
+                    telemetry.addData("Heading Data", "Current: %.1f | Target: %.1f | Difference * 100: %.1f", currentHeading, targetMoveHeading, 100*(targetMoveHeading-currentHeading));
                 } else {
-                    currentState = testStates.NONE;
+                    currentState = controlState.NONE;
                 }
-            case SAVE_POS:
-                targetMoveX = currentX + deltaX;
-                targetMoveY = currentY + deltaY;
-                targetMoveHeading = currentHeading + deltaHeading;
-                currentState = testStates.MOVE_TO_POS;
-        }
-        telemetry.addData("X Data", "Current: %.1f | Target: %.1f", currentX, targetMoveX);
-        telemetry.addData("Y Data", "Current: %.1f | Target: %.1f", currentY, targetMoveY);
-        telemetry.addData("Heading Data", "Current: %.1f | Target: %.1f", currentHeading, targetMoveHeading);
+            case MOVE_TO_CHAIN:
+                if (chains.length == 0) {
+                    currentState = controlState.NONE;
+                    break;
+                }
+                double[][][] chainResult = chain(chains, chainsError);
+                if (chainResult[0][0].length > 0) {
+                    chains = chainResult[0];
+                    chainsError = chainResult[1];
+                    telemetry.addLine("Moving to Position...");
+                    telemetry.addData("X Data", "Current: %.1f | Target: %.1f | Difference * 100: %.1f", currentX, chains[0][0], 100*(chains[0][0]-currentX));
+                    telemetry.addData("Y Data", "Current: %.1f | Target: %.1f | Difference * 100: %.1f", currentY, chains[0][1], 100*(chains[0][1]-currentY));
+                    telemetry.addData("Heading Data", "Current: %.1f | Target: %.1f | Difference * 100: %.1f", currentHeading, chains[0][2], 100*(chains[0][2]-currentHeading));
+                    telemetry.addData("Chains Data", "Length: %.1f |", chains.length);
+                } else {
+                    currentState = controlState.NONE;
+                }
         
         telemetry.update();
-
         
     }
+}
 }
